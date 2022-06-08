e93c9940a396abe226701407d82f90b3f67167cf
annastacia <annastaciangina96@gmail.com>
2022-06-08 22:40:40 +0300

    commit tree c511d2b87e32e3bc1af9a3f90ec0c49a8e2d4deb

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+
+const distanceFromHqInBlocks = (someValue) => someValue>42?someValue-42:42-someValue;
+    //returns the number of blocks given a value
+  
+
+const distanceFromHqInFeet=(someValue) => distanceFromHqInBlocks(someValue)*264;
+
+
+ const distanceTravelledInFeet=(start,destination) => destination>start?(destination-start)*264:(start-destination)*264;
+
+ 
+ const calculatesFarePrice=(start,destination) =>{
+     let charges
+     if(distanceTravelledInFeet(start,destination)<400){
+         charges=0;
+     }else if(distanceTravelledInFeet(start,destination)>400 && distanceTravelledInFeet(start,destination)<=2000){
+         charges=(distanceTravelledInFeet(start,destination)-400)*0.02;
+     }
+     else if(distanceTravelledInFeet(start,destination)>2000 && distanceTravelledInFeet(start,destination)<=2500){
+        charges=25;
+    }
+    else{
+        charges='cannot travel that far'
+    }
+    return charges;
+ }
+
+
+ 
